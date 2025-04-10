b7e6117f979fec4a3546f2b805ea0c6ce86e7894
nayana266 <nayananona@gmail.com>
2025-04-09 18:45:19 -0700

    second page

--- src/Image.js
+++ src/Image.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import img from './background.jpeg'
+import img from './background.jpeg';
 
-function Image(){
-    return(
-        <div>
-            <img src={img} alt=""/>
-        </div>
-    )
+function Image() {
+  return (
+    <div>
+      <img src={img} alt="background" className="start-image" />
+    </div>
+  );
 }
 
 export default Image;
--- src/NewPage.js
+++ src/NewPage.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import './style.css';
-import Image from './Image'; // Import the Image component
 import { Label } from './Label';
+
+import leaf from './plants/leaf.png';
+import cactus from './plants/cactus.png';
+import succulent from './plants/succulent.png';
+import blueberry from './plants/blueberry.png';
+import confettiBackground from './background.jpeg';
+
 
 function NewPage() {
   return (
     <div className="newpage-container">
-      <Image /> {/* Include the image */}
-      <Label /> {}
-      
+      <Label />
+
+      <div className="confetti-wrapper">
+        <div
+          className="confetti-box"
+          style={{ backgroundImage: `url(${confettiBackground})` }}
+        >
+          <div className="icon-grid">
+            <div className="icon-item">
+              <img src={leaf} alt="Leaf Icon" className="icon-img" />
+              <button className="icon-btn">word blast</button>
+            </div>
+            <div className="icon-item">
+              <img src={cactus} alt="Cactus Icon" className="icon-img" />
+              <button className="icon-btn">sound match</button>
+            </div>
+            <div className="icon-item">
+              <img src={succulent} alt="Succulent Icon" className="icon-img" />
+              <button className="icon-btn">rhyme racer</button>
+            </div>
+            <div className="icon-item">
+              <img src={blueberry} alt="Blueberry Icon" className="icon-img" />
+              <button className="icon-btn">story mode</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
